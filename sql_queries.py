163576167f0b99db7e059372bc8884fc68a42c38
margo-dubovik <margodubovik531@gmail.com>
2022-08-04 17:42:53 +0300

    sql_queries finished

--- sql_queries.py
+++ sql_queries.py
@@ -10,12 +10,14 @@ Session = sessionmaker(bind=engine)
 
 
 # Find all groups with less or equals student count.
-def find_by_count(count):
+def find_groups_by_students_count(count):
     with Session() as session:
         subq = select(Group.name, func.count(Student.group_id).label('n_students')) \
             .join(Student).group_by(Group.id).subquery()
         students_counted = session.query(subq).filter(subq.c.n_students <= count).all()
-    print(students_counted)
+        print("-"*70)
+        print(f"groups with <={count} students:")
+        print(students_counted)
 
 
 # Find all students related to the course with a given name.
@@ -23,6 +25,8 @@ def students_on_course(coursename):
     with Session() as session:
         students_lst = session.query(Student.first_name, Student.last_name).join(association_table).join(Course) \
             .filter(Course.name == coursename).all()
+        print("-" * 70)
+        print(f"students on {coursename} course:")
         print(students_lst)
 
 
@@ -34,22 +38,58 @@ def add_new_student(first_name, last_name, group_id, courses_ids):
                               courses=[session.query(Course).get(course_id) for course_id in courses_ids])
         session.add(new_student)
         session.commit()
+        print("-" * 70)
+        print("New student added:", new_student.first_name, new_student.last_name)
 
 
 # Delete student by STUDENT_ID
 def remove_student(the_id):
-    pass
+    with Session() as session:
+        the_student = session.query(Student).get(the_id)
+        student_courses = the_student.courses
+        # print("the_student=", the_student)
+        # print("student_courses=", student_courses)
+        for course in student_courses:
+            # print("course=", course)
+            course.students.remove(the_student)
+        session.delete(the_student)
+        session.commit()
+        print("-" * 70)
+        print(f"Student with id {the_id } successfully removed")
 
 
 # Add a student to the course (from a list)
+def add_student_to_course(student_id, course_id):
+    with Session() as session:
+        the_course = session.query(Course).get(course_id)
+        the_student = session.query(Student).get(student_id)
+        the_course.students.append(the_student)
+        session.commit()
+        print("-" * 70)
+        print(f"Student {the_student} added to course {the_course}")
+        print(f"This student's courses are:{the_student.courses}")
+
 
 # Remove the student from one of his or her courses
+def remove_student_from_course(student_id, course_id):
+    with Session() as session:
+        the_student = session.query(Student).get(student_id)
+        student_courses = the_student.courses
+        the_course = session.query(Course).get(course_id)
+        the_course.students.remove(the_student)
+        session.commit()
+        print("-" * 70)
+        print(f"Student {the_student} removed from course {the_course}")
+        print(f"This student's courses are:{the_student.courses}")
+
 
 if __name__ == '__main__':
+    find_groups_by_students_count(21)
+    students_on_course("Discrete Math")
+    add_new_student(first_name="Harry", last_name="Styles", group_id=10, courses_ids=[4, 6, 7])
+    add_student_to_course(201, 5)
+    remove_student_from_course(201, 5)
     remove_student(201)
-    # find_by_count(25)
-    # students_on_course("Discrete Math")
-    # add_new_student(first_name="Harry", last_name="Styles", group_id=10, courses_ids=[4, 6, 7])
 
 # number of students in each group:  (group № 10 is empty)
 # [(8, 22), (9, 23), (7, 27), (1, 12), (5, 20), (4, 28), (2, 29), (6, 26), (3, 13)]
