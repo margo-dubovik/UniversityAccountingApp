2d3a1915bd143c45f2d0eb91b469cc40f712975e
margo-dubovik <margodubovik531@gmail.com>
2022-08-07 22:50:18 +0300

    tests for sql almost done

--- sql_queries.py
+++ sql_queries.py
@@ -4,13 +4,11 @@ from sqlalchemy import func, select, subquery
 
 from orm_sqlalchemy.models import Group, association_table, Student, Course
 
-engine = create_engine('postgresql+psycopg2://postgres:default@localhost:5432/university_db', echo=True)
 
-Session = sessionmaker(bind=engine)
 
 
 # Find all groups with less or equals student count.
-def find_groups_by_students_count(count):
+def find_groups_by_students_count(Session, count):
     with Session() as session:
         subq = select(Group.name, func.count(Student.group_id).label('n_students')) \
             .join(Student).group_by(Group.id).subquery()
@@ -19,17 +17,15 @@ def find_groups_by_students_count(count):
 
 
 # Find all students related to the course with a given name.
-def students_on_course(coursename):
+def students_on_course(Session, coursename):
     with Session() as session:
         students_lst = session.query(Student.first_name, Student.last_name).join(association_table).join(Course) \
             .filter(Course.name == coursename).all()
-        print("-" * 70)
-        print(f"students on {coursename} course:")
-        print(students_lst)
+    return students_lst
 
 
 # Add new student
-def add_new_student(first_name, last_name, group_id, courses_ids):
+def add_new_student(Session, first_name, last_name, group_id, courses_ids):
     with Session() as session:
         max_id = session.query(func.max(Student.id)).scalar()
         new_student = Student(id=max_id + 1, first_name=first_name, last_name=last_name, group_id=group_id,
@@ -41,7 +37,7 @@ def add_new_student(first_name, last_name, group_id, courses_ids):
 
 
 # Delete student by STUDENT_ID
-def remove_student(the_id):
+def remove_student(Session, the_id):
     with Session() as session:
         the_student = session.query(Student).get(the_id)
         student_courses = the_student.courses
@@ -57,7 +53,7 @@ def remove_student(the_id):
 
 
 # Add a student to the course (from a list)
-def add_student_to_course(student_id, course_id):
+def add_student_to_course(Session, student_id, course_id):
     with Session() as session:
         the_course = session.query(Course).get(course_id)
         the_student = session.query(Student).get(student_id)
@@ -69,7 +65,7 @@ def add_student_to_course(student_id, course_id):
 
 
 # Remove the student from one of his or her courses
-def remove_student_from_course(student_id, course_id):
+def remove_student_from_course(Session, student_id, course_id):
     with Session() as session:
         the_student = session.query(Student).get(student_id)
         student_courses = the_student.courses
@@ -82,15 +78,23 @@ def remove_student_from_course(student_id, course_id):
 
 
 if __name__ == '__main__':
-    groups_counted = find_groups_by_students_count(21)
+    engine = create_engine('postgresql+psycopg2://postgres:default@localhost:5432/university_db', echo=True)
+
+    Session = sessionmaker(bind=engine)
+    groups_counted = find_groups_by_students_count(Session, 21)
     print("-" * 70)
     print(f"groups with <=21 students:")
     print(groups_counted)
-    # students_on_course("Discrete Math")
-    # add_new_student(first_name="Harry", last_name="Styles", group_id=10, courses_ids=[4, 6, 7])
-    # add_student_to_course(201, 5)
-    # remove_student_from_course(201, 5)
-    # remove_student(201)
+
+    students_lst = students_on_course("Discrete Math")
+    print("-" * 70)
+    print(f"students on Discrete Math course:")
+    print(students_lst)
+
+    # add_new_student(Session, first_name="Harry", last_name="Styles", group_id=10, courses_ids=[4, 6, 7])
+    # add_student_to_course(Session, 201, 5)
+    # remove_student_from_course(Session, 201, 5)
+    # remove_student(Session, 201)
 
 # number of students in each group:  (group № 10 is empty)
 # [(8, 22), (9, 23), (7, 27), (1, 12), (5, 20), (4, 28), (2, 29), (6, 26), (3, 13)]
--- tests/test_sql_queries.py
+++ tests/test_sql_queries.py
@@ -1,66 +1,123 @@
-import pytest
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, scoped_session
-
-from sql_queries import *
-from orm_sqlalchemy.models import Base, Group, association_table, Student, Course
-from orm_sqlalchemy.generator_source import group_names, course_names
-
-
-@pytest.fixture(scope="session")
-def connection():
-    print("\nCONNECTING\n")
-    engine = create_engine("postgresql+psycopg2://postgres:default@localhost:5433/test_university_db")
-    return engine.connect()
-
-
-@pytest.fixture(scope="session")
-def setup_database(connection):
-    Base.metadata.bind = connection
-    Base.metadata.create_all()
-
-    seed_database()
-
-    yield
-
-    Base.metadata.drop_all()
-
-
-@pytest.fixture
-def db_session(setup_database, connection):
-    transaction = connection.begin()
-    yield scoped_session(
-        sessionmaker(autocommit=False, autoflush=False, bind=connection)
-    )
-    transaction.rollback()
-
-
-def seed_database():
-
-    db_session.add_all([Group(id=i, name=name) for i, name in enumerate(group_names[:3], 1)])
-    db_session.commit()
-
-    db_session.add_all(
-        [Course(id=i, name=course[0], description=course[1]) for i, course in enumerate(course_names[:4], 1)])
-    db_session.commit()
-
-    groups_dict = {"FI-83":
-                   [['Alice', 'Evans',[1]], ['Helen', 'Turner',[2,4]], ['Jack', 'Fox',[1,3]], ['Leo', 'Fisher',[3]]],
-                   "FB-61":
-                   [['Andrew', 'Turner',[2,3]], ['Jane', 'Garfield',[1,3,4]], ['Harry', 'Styles',[4]]],
-                   "SA-72":
-                   [['Alan', 'Turing',[1,2,3]], ['Sophia', 'Roberts', [1,4]]]}
-
-    i = 0
-    for group_number, groupname in enumerate(groups_dict.keys(), 1):
-        for student in groups_dict[groupname]:
-            courses = student[2]
-            db_session.add(Student(id=i + 1, first_name=student[0], last_name=student[1], group_id=group_number,
-                                courses=[db_session.query(Course).get(course_id) for course_id in courses]))
-    db_session.commit()
-
-
-def test_find_groups_by_students_count(db_session):
-    print("\nCOUNTING GROUPS\n")
-    groups_counted = find_groups_by_students_count(3)
-    assert groups_counted == [("FB-61", 3), ("SA-72", 2)]
+# import pytest
+# from sqlalchemy import create_engine
+# from sqlalchemy.orm import sessionmaker, scoped_session
+#
+# from sql_queries import *
+# from orm_sqlalchemy.models import Base, Group, association_table, Student, Course
+# from orm_sqlalchemy.generator_source import group_names, course_names
+#
+#
+# @pytest.fixture(scope="session")
+# def connection():
+#     engine = create_engine("postgresql+psycopg2://postgres:default@localhost:5432/test_university_db")
+#     return engine.connect()
+#
+#
+# @pytest.fixture(scope="session")
+# def setup_database(connection):
+#     Base.metadata.bind = connection
+#     Base.metadata.create_all()
+#
+#     # seed_database()
+#
+#     yield
+#
+#     Base.metadata.drop_all()
+#
+#
+# @pytest.fixture
+# def db_session(setup_database, connection):
+#     transaction = connection.begin()
+#     session = scoped_session(
+#         sessionmaker(autocommit=False, autoflush=False, bind=connection)
+#     )
+#     # session = Session(bind=connection)
+#     yield session
+#
+#     session.close()
+#     transaction.rollback()
+#     connection.close()
+#
+#
+# def seed_database():
+#     engine = create_engine("postgresql+psycopg2://postgres:default@localhost:5432/test_university_db")
+#     connection = engine.connect()
+#     with Session(bind=connection) as session:
+#
+#     # with Session() as session:
+#         session.add_all([Group(id=i, name=name) for i, name in enumerate(group_names[:3], 1)])
+#
+#         session.commit()
+#
+#         session.add_all(
+#             [Course(id=i, name=course[0], description=course[1]) for i, course in enumerate(course_names[:4], 1)])
+#         session.commit()
+#
+#         groups_dict = {"FI-83":
+#                        [['Alice', 'Evans',[1]], ['Helen', 'Turner',[2,4]], ['Jack', 'Fox',[1,3]], ['Leo', 'Fisher',[3]]],
+#                        "FB-61":
+#                        [['Andrew', 'Turner',[2,3]], ['Jane', 'Garfield',[1,3,4]], ['Harry', 'Styles',[4]]],
+#                        "SA-72":
+#                        [['Alan', 'Turing',[1,2,3]], ['Sophia', 'Roberts', [1,4]]]}
+#
+#         i = 0
+#         for group_number, groupname in enumerate(groups_dict.keys(), 1):
+#             for student in groups_dict[groupname]:
+#                 courses = student[2]
+#                 session.add(Student(id=i + 1, first_name=student[0], last_name=student[1], group_id=group_number,
+#                                     courses=[session.query(Course).get(course_id) for course_id in courses]))
+#                 i += 1
+#
+#         session.commit()
+#
+#
+#
+# def test_find_groups_by_students_count(db_session):
+#     # seed_database()
+#     print("\nCOUNTING GROUPS\n")
+#     count = 3
+#     with db_session as session:
+#         subq = select(Group.name, func.count(Student.group_id).label('n_students')) \
+#             .join(Student).group_by(Group.id).subquery()
+#     groups_counted = session.query(subq).filter(subq.c.n_students <= count).all()
+#     # groups_counted = find_groups_by_students_count()
+#     assert groups_counted == [("FB-61", 3), ("SA-72", 2)]
+#
+# #
+# #
+# # def test_find_groups_by_students_count(db_session):
+# #     seed_database(db_session)
+# #     print("\nCOUNTING GROUPS\n")
+# #     groups_counted = find_groups_by_students_count(3)
+# #     assert groups_counted == [("FB-61", 3), ("SA-72", 2)]
+#
+# #
+# # @pytest.fixture(scope="session")
+# # def engine():
+# #     return create_engine("postgresql+psycopg2://postgres:default@localhost:5432/test_university_db")
+# #
+# #
+# # @pytest.fixture(scope="session")
+# # def tables(engine):
+# #     Base.metadata.bind = engine.connect()
+# #     Base.metadata.create_all()
+# #     yield
+# #     Base.metadata.drop_all(engine)
+# #
+# #
+# # @pytest.fixture
+# # def db_session(engine, tables):
+# #     """Returns an sqlalchemy session, and after the test tears down everything properly."""
+# #     connection = engine.connect()
+# #     # begin the nested transaction
+# #     transaction = connection.begin()
+# #     # use the connection with the already started transaction
+# #     session = Session(bind=connection)
+# #
+# #     yield session
+# #
+# #     session.close()
+# #     # roll back the broader transaction
+# #     transaction.rollback()
+# #     # put back the connection to the connection pool
+# #     connection.close()
