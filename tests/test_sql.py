9f7f0b3ba675b6aed3957b88ac5efabbe554d018
margo-dubovik <margodubovik531@gmail.com>
2022-08-07 23:01:03 +0300

    tests for sql  done

--- tests/test_sql.py
+++ tests/test_sql.py
@@ -5,35 +5,33 @@ from orm_sqlalchemy.models import Base, Group, association_table, Student, Cours
 from orm_sqlalchemy.generator_source import group_names, course_names
 
 
-# def seed_database():
-#     engine = create_engine("postgresql+psycopg2://postgres:default@localhost:5432/test_university_db")
-#     connection = engine.connect()
-#     with Session(bind=connection) as session:
-#         session.add_all([Group(id=i, name=name) for i, name in enumerate(group_names[:3], 1)])
-#
-#         session.commit()
-#
-#         session.add_all(
-#             [Course(id=i, name=course[0], description=course[1]) for i, course in enumerate(course_names[:4], 1)])
-#         session.commit()
-#
-#         groups_dict = {"FI-83":
-#                            [['Alice', 'Evans', [1]], ['Helen', 'Turner', [2, 4]], ['Jack', 'Fox', [1, 3]],
-#                             ['Leo', 'Fisher', [3]]],
-#                        "FB-61":
-#                            [['Andrew', 'Turner', [2, 3]], ['Jane', 'Garfield', [1, 3, 4]], ['Harry', 'Styles', [4]]],
-#                        "SA-72":
-#                            [['Alan', 'Turing', [1, 2, 3]], ['Sophia', 'Roberts', [1, 4]]]}
-#
-#         i = 0
-#         for group_number, groupname in enumerate(groups_dict.keys(), 1):
-#             for student in groups_dict[groupname]:
-#                 courses = student[2]
-#                 session.add(Student(id=i + 1, first_name=student[0], last_name=student[1], group_id=group_number,
-#                                     courses=[session.query(Course).get(course_id) for course_id in courses]))
-#                 i += 1
-#
-#         session.commit()
+def seed_database():
+    with Session(bind=connection) as session:
+        session.add_all([Group(id=i, name=name) for i, name in enumerate(group_names[:3], 1)])
+
+        session.commit()
+
+        session.add_all(
+            [Course(id=i, name=course[0], description=course[1]) for i, course in enumerate(course_names[:4], 1)])
+        session.commit()
+
+        groups_dict = {"FI-83":
+                           [['Alice', 'Evans', [1]], ['Helen', 'Turner', [2, 4]], ['Jack', 'Fox', [1, 3]],
+                            ['Leo', 'Fisher', [3]]],
+                       "FB-61":
+                           [['Andrew', 'Turner', [2, 3]], ['Jane', 'Garfield', [1, 3, 4]], ['Harry', 'Styles', [4]]],
+                       "SA-72":
+                           [['Alan', 'Turing', [1, 2, 3]], ['Sophia', 'Roberts', [1, 4]]]}
+
+        i = 0
+        for group_number, groupname in enumerate(groups_dict.keys(), 1):
+            for student in groups_dict[groupname]:
+                courses = student[2]
+                session.add(Student(id=i + 1, first_name=student[0], last_name=student[1], group_id=group_number,
+                                    courses=[session.query(Course).get(course_id) for course_id in courses]))
+                i += 1
+
+        session.commit()
 
 
 # @pytest.fixture
@@ -82,12 +80,11 @@ from orm_sqlalchemy.generator_source import group_names, course_names
 engine = create_engine("postgresql+psycopg2://postgres:default@localhost:5432/test_university_db")
 connection = engine.connect()
 Base.metadata.bind = connection
-# Base.metadata.create_all(engine)
+Base.metadata.create_all(engine)
 Session = sessionmaker(bind=engine)
-# setup_db(the_session)
+seed_database()
 
 def test_find_groups_by_students_count():
-    remove_student(Session, 10)
     count = 3
     groups_counted = find_groups_by_students_count(Session, count)
     assert groups_counted == [("FB-61", 3), ("SA-72", 2)]
@@ -110,5 +107,19 @@ def test_add_student_to_course():
         discr_math = session.query(Course).filter_by(id=1).first()
         print("discr_math.students=", discr_math.students)
         assert steve in discr_math.students
+
+def test_remove_student_from_course():
+    remove_student_from_course(Session, 10, 1)
+    with Session() as session:
+        steve = session.query(Student).filter_by(first_name="Steve", last_name="Rogers").first()
+        discr_math = session.query(Course).filter_by(id=1).first()
+        print("discr_math.students=", discr_math.students)
+        assert steve not in discr_math.students
+
+def test_remove_student():
+    remove_student(Session, 10)
+    with Session() as session:
+        n_steves = session.query(Student).filter_by(first_name="Steve", last_name="Rogers").count()
+        assert n_steves == 0
 
 # Base.metadata.drop_all()
